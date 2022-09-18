312012c78400ab71b431a74c3ee3464faab2a1ac
Pavel Ilchenko <arisesinmight@gmail.com>
2022-09-18 12:34:37 +0300

    Check fixed quest strings

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -13,7 +13,7 @@ def build_quest():
         counter += 1
     progression_tuple = progression.split(' ')
     right_answer = random.choice(progression_tuple)
-    question = progression.replace(right_answer, '..')
+    question = f'{progression.replace(right_answer, '..')}'
     ask = 'What number is missing in the progression?'
     return question, right_answer, ask
 
