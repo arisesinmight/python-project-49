487ea755dfa0bcd88e7dcdf2e4f5f8c3002c36f9
Pavel Ilchenko <arisesinmight@gmail.com>
2022-09-18 13:29:13 +0300

    Recheck

--- brain_games/games/is_prime.py
+++ brain_games/games/is_prime.py
@@ -4,7 +4,7 @@ import sympy
 
 def build_quest():
     ask = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-    question = f'{random.randint(1, 100)}'
+    question = random.randint(1, 100)
     if sympy.isprime(int(question)) is True:
         right_answer = 'yes'
     else:
